6e1de3ed7b399d4798809e5036c54244e215cd83
Betta_Fish <zxbmmmmmmmmm@qq.com>
2025-12-23 13:37:28 +0800

    feat: interaction tracker

---
+++ src/Avalonia.Base/Rendering/Composition/Interaction/InputElementInteractionSource.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia.Input;
+
+namespace Avalonia.Rendering.Composition.Interaction;
+
+public class InputElementInteractionSource
+{
+    private IPointer? _firstPointer;
+    private IPointer? _secondPointer;
+    public InputElementInteractionSource(InputElement inputElement)
+    {
+        inputElement.PointerPressed += OnPointerPressed;
+        inputElement.PointerMoved += OnPointerMoved;
+    }
+
+    private void OnPointerMoved(object? sender, PointerEventArgs e)
+    {
+        
+        
+    }
+
+    private void OnPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        e.Pointer
+    }
+}
---
+++ src/Avalonia.Base/Rendering/Composition/Interaction/ServerInteractionTracker.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Rendering.Composition.Server;
+
+namespace Avalonia.Rendering.Composition.Interaction;
+
+internal class ServerInteractionTracker : ServerObject
+{
+    public ServerInteractionTracker(ServerCompositor compositor) : base(compositor)
+    {
+
+    }
+}
