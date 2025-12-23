b4b5c1128bc77c46f820a1fb7983656e0eb6c343
Betta_Fish <zxbmmmmmmmmm@qq.com>
2025-12-23 18:35:17 +0800

    feat: add interaction source

--- src/Avalonia.Base/Rendering/Composition/Interaction/InputElementInteractionSource.cs
+++ src/Avalonia.Base/Rendering/Composition/Interaction/InputElementInteractionSource.cs
@@ -1,22 +1,38 @@
-﻿using Avalonia.Input;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Avalonia.Input;
 
 namespace Avalonia.Rendering.Composition.Interaction;
 
 public class InputElementInteractionSource
 {
-    public InputElementInteractionSource(InputElement inputElement)
+    private InteractionTracker _tracker;// TODO: Support multiple trackers
+    public InputElementInteractionSource(InputElement inputElement, InteractionTracker tracker)
     {
         inputElement.PointerPressed += OnPointerPressed;
         inputElement.PointerMoved += OnPointerMoved;
+        inputElement.PointerWheelChanged += OnPointerWheelChanged;
+        _tracker = tracker;
+    }
+
+    private void OnPointerWheelChanged(object? sender, PointerWheelEventArgs e)
+    {
+        if(e.Delta.Y != 0)
+        {
+            _tracker.ReceivePointerWheel((int)e.Delta.Y, false);
+        }
+        else
+        {
+            _tracker.ReceivePointerWheel((int)e.Delta.X, true);
+        }
     }
 
     private void OnPointerMoved(object? sender, PointerEventArgs e)
     {
-        
-        
     }
 
     private void OnPointerPressed(object? sender, PointerPressedEventArgs e)
     {
+        _tracker.StartUserManipulation();
     }
 }
