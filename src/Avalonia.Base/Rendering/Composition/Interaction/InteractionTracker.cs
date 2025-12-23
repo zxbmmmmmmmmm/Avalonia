8c068f981ac4ad714f6dde9cdb002c12fbe9c2a7
Betta_Fish <zxbmmmmmmmmm@qq.com>
2025-12-23 13:37:28 +0800

    feat: interaction tracker

---
+++ src/Avalonia.Base/Rendering/Composition/Interaction/InteractionTracker.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia.Rendering.Composition.Server;
+using Avalonia.Styling;
+
+namespace Avalonia.Rendering.Composition.Interaction;
+
+public class InteractionTracker : CompositionObject
+{
+    private long _requestId = 0;
+    public IInteractionTrackerOwner? Owner { get; }
+    internal new ServerInteractionTracker? Server { get; }
+    
+    internal InteractionTracker(Compositor compositor, ServerInteractionTracker server) : base(compositor, server)
+    {
+        Server = server;
+    }
+    internal InteractionTracker(Compositor compositor, ServerInteractionTracker server, IInteractionTrackerOwner owner) : base(compositor, new ServerInteractionTracker(compositor.Server))
+    {
+        Server = server;
+        Owner = owner;
+    }
+}
---
+++ src/Avalonia.Base/Rendering/Composition/Interaction/InteractionTrackerState.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+
+namespace Avalonia.Rendering.Composition.Interaction;
+
+internal abstract class InteractionTrackerState
+{
+    private protected ServerInteractionTracker _interactionTracker;
+    private protected bool _disposed;
+
+    protected InteractionTrackerState(ServerInteractionTracker interactionTracker)
+    {
+        _interactionTracker = interactionTracker;
+        // ReSharper disable once VirtualMemberCallInConstructor
+        EnterState(interactionTracker.Owner);
+    }
+
+    protected abstract void EnterState(IInteractionTrackerOwner? owner);
+    internal abstract void StartUserManipulation();
+    internal abstract void CompleteUserManipulation(Vector3 linearVelocity);
+    internal abstract void ReceiveManipulationDelta(Point translationDelta);
+    internal abstract void ReceiveInertiaStarting(Point linearVelocity);
+    internal abstract void ReceivePointerWheel(int delta, bool isHorizontal);
+    internal abstract void TryUpdatePositionWithAdditionalVelocity(Vector3 velocityInPixelsPerSecond, int requestId);
+    internal abstract void TryUpdatePosition(Vector3 value, InteractionTrackerClampingOption option, int requestId);
+}
