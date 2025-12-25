19673eb6aa9b342101e99304cdd3f3a3734a83e0
Betta_Fish <zxbmmmmmmmmm@qq.com>
2025-12-25 20:59:43 +0800

    fix: clamp

--- src/Avalonia.Base/Rendering/Composition/Server/ServerObject.cs
+++ src/Avalonia.Base/Rendering/Composition/Server/ServerObject.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Avalonia.Rendering.Composition.Animations;
