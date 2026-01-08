a0e1e77bbb18b8036e79be5bc91d545c36dbcd53
Betta_Fish <zxbmmmmmmmmm@qq.com>
2026-01-08 13:37:08 +0800

    chore: add todo

--- src/Avalonia.Base/Visual.Composition.cs
+++ src/Avalonia.Base/Visual.Composition.cs
@@ -135,7 +135,7 @@ public partial class Visual
 
         // TODO: Introduce a dirty mask like WPF has, so we don't overwrite properties every time
 
-        if (!comp.Server.HasAnimationForProperty("Offset"))
+        if (!comp.Server.HasAnimationForProperty("Offset"))// TODO: Remove this once we have Translation property
         {
             comp.Offset = new(Bounds.Left, Bounds.Top, 0);
         }
